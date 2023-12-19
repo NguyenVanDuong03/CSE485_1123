9ed928c4c27169056936004f7383e90cc68407ae
NguyenVanDuong03 <19.nguyenvanduong@gmail.com>
2023-12-19 20:56:29 +0700

    update

--- BTTH03/app/config/connect.php
+++ BTTH03/app/config/connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $conn = new PDO("mysql:host=localhost;dbname=Quanlysinhvien", "root", "123");
+  $conn = new PDO("mysql:host=localhost;dbname=LMS", "root", "123");
 } catch (PDOException $e) {
   die("Lỗi kết nối: " . $e->getMessage());
 }
